50fe85530e1b2282358e1a8846451e7a42e8d61f
MadFlasheroo7 <jayeshseth45@gmail.com>
2022-07-04 02:08:09 +0500

    buildSrc: clean gradle

--- app/src/main/java/xyz/jayeshseth/budgetmanager/ui/theme/Color.kt
+++ app/src/main/java/xyz/jayeshseth/budgetmanager/ui/theme/Color.kt
@@ -2,7 +2,21 @@ package xyz.jayeshseth.budgetmanager.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val DarkGrey900 = Color(0xFF18191E)
+val DarkGrey800 = Color(0xFF1E1F24)
+val DarkGrey700 = Color(0xFF26282F)
+val PastelGreen = Color(0xFFB6FBA4)
+
+val DarkBlue800 = Color(0xFF262446)
+val DarkBlue600 = Color(0xFF2D2B4F)
+val DarkBlue500 = Color(0xFF39375B)
+val Teal500 = Color(0xFF1DD3B0)
+
+
+val DarkPurple800 = Color(0xFF26081C)
+val DarkPurple700 = Color(0xFF2F002D)
+val DarkPurple600 = Color(0xFF360834)
+val DarkPurple200 = Color(0xFFDC6ACF)
+
+
 val Teal200 = Color(0xFF03DAC5)
--- app/src/main/java/xyz/jayeshseth/budgetmanager/ui/theme/Theme.kt
+++ app/src/main/java/xyz/jayeshseth/budgetmanager/ui/theme/Theme.kt
@@ -2,37 +2,63 @@ package xyz.jayeshseth.budgetmanager.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+
+private val DeepBlueTheme = lightColors(
+    primary = DarkBlue800,
+    primaryVariant = DarkBlue800,
+    secondary = Teal500,
+    onSecondary = DarkBlue500,
+    background = DarkBlue500,
+    onBackground = Color.White,
+    surface = DarkBlue600,
+    onSurface = Color.White
+
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val DeepPurpleTheme = lightColors(
+    primary = DarkPurple800,
+    primaryVariant = DarkPurple800,
+    secondary = DarkPurple200,
+    onSecondary = DarkPurple600,
+    background = DarkPurple600,
+    onBackground = Color.White,
+    surface = DarkPurple700,
+    onSurface = Color.White
+)
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val DarkGrayTheme = lightColors(
+    primary = DarkGrey900,
+    primaryVariant = DarkGrey900,
+    secondary = PastelGreen,
+    onSecondary = DarkGrey700,
+    background = DarkGrey700,
+    onBackground = Color.White,
+    surface = DarkGrey800,
+    onSurface = Color.White
+
 )
 
 @Composable
-fun BudgetManagerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun BudgetManagerTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+
     val colors = if (darkTheme) {
-        DarkColorPalette
+        DarkGrayTheme
     } else {
-        LightColorPalette
+//        val systemUiController = rememberSystemUiController()
+//        SideEffect {
+//            systemUiController.setStatusBarColor(
+//                color = DarkPurple600
+//            )
+//        }
+//        DeepPurpleTheme
+        DeepBlueTheme
     }
 
     MaterialTheme(
